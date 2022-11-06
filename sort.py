591a02469ce51decbdbf56b174190bafa31af0bd
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-11-06 22:16:29 +0330

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr=np.array([3,2,0,1])
+fruits=np.array(['banana','cherry','apple'])
+arr2=np.array([True,False,True,False])
+arr3=np.array([
+    [3,2,4],
+    [5,0,1]
+])
+
+print(np.sort(arr))
+print(np.sort(arr2))
+print(np.sort(arr3))
+print(np.sort(fruits))
