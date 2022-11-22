9936d89226e75fc334f634b2554833a53ce06f9b
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-11-22 21:32:43 +0330

    Create Ufunc_simple_arithmetic.py

---
+++ Ufunc_simple_arithmetic.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr1=np.array([10,20,30,40,50,60])
+arr=np.array([1,-2,3,-4])
+arr2=np.array([3,7,9,8,2,33])
+
+# print(np.add(arr1,arr2))
+# print(np.subtract(arr2,arr1))
+# print(np.multiply(arr1,arr2))
+# print(np.divide(arr2,arr1))
+# print(np.power(arr1,arr2))
+# print(np.mod(arr1,arr2))
+# print(np.remainder(arr1,arr2))
+# print(np.divmod(arr1,arr2))
+print(np.absolute(arr))
