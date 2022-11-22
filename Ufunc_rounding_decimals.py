347a5d4dfd81b86d3ec4d0964a42e9223e55d2e3
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2022-11-22 21:42:10 +0330

    Create Ufunc_rounding_decimals.py

---
+++ Ufunc_rounding_decimals.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr=np.trunc([-3.16666,3.66667])
+arr=np.fix([-3.16666,3.66667])
+arr=np.around(3.1666,2)
+arr=np.floor([-3.1666,3.6667])
+arr=np.ceil([-3.1666,3.6667])
+print(arr)
